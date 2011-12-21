bdfcb0128231369fb0919d0728abda83c8ff2083
Diego Salazar <diego@greyrobot.com>
2011-12-21 10:27:13 -0500

    added seed option

--- conwaysgame.js
+++ conwaysgame.js
@@ -1,5 +1,43 @@
 // http://en.wikipedia.org/wiki/Conway's_Game_of_Life
 
+$.cookie = function() {
+  function get(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    
+    for (var i=0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+  
+  function set(name,value,days) {
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      var expires = "; expires="+date.toGMTString();
+    } else {var expires = "";}
+    
+    document.cookie = name+"="+value+expires+"; path=/";
+  }
+  
+  function xdelete(name) {
+    set(name, null, -1);
+  }
+  
+  if (arguments.length == 1) {
+    return get(arguments[0]);
+    
+  } else if (arguments[1] == null) {
+    xdelete(arguments[0]);
+    
+  } else {
+    set(arguments[0], arguments[1], arguments[2]);
+  }
+}  
+
 // represents the grid, updates cells state and draws
 function Automaton(canvas_id, w, h, seed, color) {   
   this.w = w;
@@ -31,6 +69,21 @@ Automaton.adjust = {
   'below':      { x:  0, y:  1 },
   'belowLeft':  { x: -1, y:  1 },
   'left':       { x: -1, y:  0 }
+}
+// http://en.wikipedia.org/wiki/Conway's_Game_of_Life
+Automaton.seeds = {
+  'gosper' : [[2,6],[2,7],[3,6],[3,7],[12,6],[12,7],[12,8],[13,5],[13,9],[14,4],[14,10],[15,4],[15,10],
+              [16,7],[17,5],[17,9],[18,6],[18,7],[18,8],[19,7],[22,4],[22,5],[22,6],[23,4],[23,5],[23,6],[24,3],[24,7],
+              [26,2],[26,3],[26,7],[26,8],[36,4],[36,5],[37,4],[37,5]], // thanks 535480/james from stackoverflow
+  'sword' : [[26,10], [26,11], [26,12], [26,13], [26,14], [26,15], [26,16], [26,18], [26,19], [26,20],
+             [26,23], [26,24], [26,25], [26,30], [26,31], [26,32], [26,33], [26,34], [26,35]],
+  'spaceship' : [],
+  'toad' : [],
+  'beacon' : [],
+  'pulsar' : [],
+  'lwss' : [],
+  'diehard' : [],
+  'acorn' : []
 }
 
 Automaton.prototype.traverseGrid = function(yCallback, xCallback) {
@@ -125,6 +178,15 @@ Automaton.prototype.getCell = function(x, y) {
   return this.grid[x][y];
 }
 
+// return a seed array
+Automaton.prototype.getSeed = function() {
+  var seed = [];
+  this.traverseGrid(function(cell, x, y) {
+    if (cell.alive) seed.push([x, y]);
+  });
+  return seed;
+}
+
 function Cell(x, y, alive, automaton) {
   this.x = x;
   this.y = y;
@@ -192,91 +254,156 @@ Cell.prototype.lifeColor = function() {
   return Cell.lifeColors[this.numLiveNeighbors()];
 }
 
+function Storage(name) {
+  this.name = name;
+  this.container = function() {
+    var storageType = 'cookieStore',
+        types = { 'cookieStore': $.cookie };
+    
+    return types[storageType];
+  }
+  return this;
+}
+
+Storage.prototype.save = function(name, data) {
+  c_log(name, data);
+  this.container(name, data);
+  return true;
+}
+
+Storage.prototype.read = function(name) {
+  c_log('read', name);
+  this.container(name);
+  return true;
+}
+
+function Inputs() {
+  this.names = ['fps', 'w', 'h', 'maxClumps', 'clumpSize'];
+}
+
+Inputs.prototype.state = function() {
+  var iS = {}
+  for (var i = 0, len = Inputs.length; i < len; i++) {
+    var name = Inputs[i];
+    if ($('#'+ name).val) 
+      iS[name] = parseInt($('#'+ name).val()); 
+  }
+  return iS;
+}
+
+Inputs.prototype.read = function() {
+  fps = parseInt($('#fps').val());
+  w = parseInt($('#w').val());
+  h = parseInt($('#h').val());
+  seed = $('#preSeed').val();
+  maxClumps = parseInt($('#maxClumps').val());
+  clumpSize = parseInt($('#clumpSize').val());
+}
+
+Inputs.prototype.get = function(name) {
+  return $('#'+ name).val();
+}
+
+function Anim(inputs, automaton) {
+  this.interval = 0;
+  this.inputs = inputs;
+  this.automaton = automaton;
+  this.liveCellCounter = $('#liveCellCount');
+}
+
+Anim.prototype.start = function() {
+  var self = this;
+  this.interval = setInterval(function() {
+    self.automaton.update().draw();
+    self.liveCellCounter.text(self.automaton.liveCellCount());
+  }, 1000/self.inputs.get('fps'));
+},
+
+Anim.prototype.stop = function() {
+  clearInterval(this.interval);
+  this.interval = 0;
+}
+
 $(function() {
-  var w = 40,
-      h = 30,
-      fps = 10,
-      // Gosper's Glider Gun
-      seed = [[2,6],[2,7],[3,6],[3,7],[12,6],[12,7],[12,8],[13,5],[13,9],[14,4],[14,10],[15,4],[15,10],
-      [16,7],[17,5],[17,9],[18,6],[18,7],[18,8],[19,7],[22,4],[22,5],[22,6],[23,4],[23,5],[23,6],[24,3],[24,7],
-      [26,2],[26,3],[26,7],[26,8],[36,4],[36,5],[37,4],[37,5]],
-      interval = 0,
-      canvas_id = 'grid',
-      maxClumps = 30,
-      clumpSize = 40,
-      liveCellCounter = $('#liveCellCount'),
-      automaton = new Automaton(canvas_id, w, h, seed),
-      animStart = function() {
-        readInputs();
-        
-        interval = setInterval(function() {
-          automaton.update().draw();
-          liveCellCounter.text(automaton.liveCellCount());
-        }, 1000/fps);
-      },
-      animStop = function() {
-        clearInterval(interval);
-        interval = 0;
-      };
-
-
-  $('#start').click(animStart);
-  $('#stop').click(animStop);
+  var inputs    = new Inputs(),
+      w         = inputs.get('w') || 40,
+      h         = inputs.get('h') || 30,
+      canvas_id = inputs.get('canvas_id') || 'grid',
+      maxClumps = inputs.get('maxClumps') || 30,
+      clumpSize = inputs.get('clumpSize') || 40,
+      fps       = inputs.get('fps') || 10,
+      seed      = Automaton.seeds[inputs.get('preSeed')],
+      storage   = new Storage('conways'),
+      automaton = new Automaton(canvas_id, inputs.get('w'), inputs.get('h'), seed),
+      anim      = new Anim(inputs, automaton);
+      
+  $('#start').click(function() { anim.start(); });
+  $('#stop').click(function() { anim.stop(); });
   
   $('#reset').click(function() {
-    animStop();
-    automaton = new Automaton(canvas_id, w, h, seed).draw();
-    liveCellCounter.text(automaton.liveCellCount());
+    anim.stop();
+    anim.automaton = new Automaton(canvas_id, inputs.get('w'), inputs.get('h'), seed).draw();
+    anim.liveCellCounter.text(anim.automaton.liveCellCount());
   });
   
   $('#update').submit(function() {
-    readInputs();
-    automaton = new Automaton(canvas_id, w, h, seed).draw();
-    liveCellCounter.text(automaton.liveCellCount());
+    inputs.read();
+    anim.automaton = new Automaton(canvas_id, inputs.get('w'), inputs.get('h'), seed).draw();
+    anim.liveCellCounter.text(anim.automaton.liveCellCount());
     return false;
   });
   
   $('#clear').click(function() {
-    animStop();
-    readInputs();
-    automaton = new Automaton(canvas_id, w, h).draw();
-    liveCellCounter.text(automaton.liveCellCount());
+    anim.stop();
+    inputs.read();
+    anim.automaton = new Automaton(canvas_id, inputs.get('w'), inputs.get('h')).draw();
+    anim.liveCellCounter.text(anim.automaton.liveCellCount());
   });
   
   // randomly seed the grid
   $('#random').click(function() {
-    animStop();
-    readInputs();
-    automaton = new Automaton(canvas_id, w, h).randomSeed(maxClumps, clumpSize).draw();
-    liveCellCounter.text(automaton.liveCellCount());
+    anim.stop();
+    inputs.read();
+    anim.automaton = new Automaton(canvas_id, inputs.get('w'), inputs.get('h'))
+                              .randomSeed(inputs.get('maxClumps'), inputs.get('clumpSize'))
+                              .draw();
+    anim.liveCellCounter.text(anim.automaton.liveCellCount());
+  });
+  
+  $('#save').click(function() {
+    anim.stop();
+    inputs.read();
+    storage.save('getSeed', anim.automaton.getSeed());
+    storage.save('inputState', inputs.state());
+  });
+  
+  $('#preSeed').change(function() {
+    anim.stop();
+    storage.save('preSeed', Automaton.seeds[$('#preSeed').val()]);
+    seed = Automaton.seeds[inputs.get('preSeed')];
+    anim.automaton = new Automaton(canvas_id, inputs.get('w'), inputs.get('h'), seed).draw();
   });
   
   // toggle individual cells
   $('#grid').click(function(e) {
     var canvas = $(this),
-        x = Math.floor((e.pageX - canvas.offset().left) / automaton.unit);
-        y = Math.floor((e.pageY - canvas.offset().top) / automaton.unit);
+        x = Math.floor((e.pageX - canvas.offset().left) / anim.automaton.unit);
+        y = Math.floor((e.pageY - canvas.offset().top) / anim.automaton.unit);
         cell = automaton.getCell(x, y);
     
     cell.alive = !cell.alive;
-    automaton.draw();
-    liveCellCounter.text(automaton.liveCellCount());
+    anim.automaton.draw();
+    anim.liveCellCounter.text(anim.automaton.liveCellCount());
     
     if (cell.alive) c_log(x, y);
   });
   
-  function readInputs() {
-    fps = parseInt($('#fps').val());
-    w = parseInt($('#w').val());
-    h = parseInt($('#h').val());
-    maxClumps = parseInt($('#maxClumps').val());
-    clumpSize = parseInt($('#clumpSize').val());
-  }
-  readInputs();
+  
+  inputs.read();
 });
 
 function c_log() {
-  if (console && console.log) console.log(arguments);
+  if (console.log) console.log(arguments);
 }
 
 function rand(num) {
